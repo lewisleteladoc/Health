0be5617d38263fdcbae9ebcdc1812f9c7f8f1170
Lewis Le <haile819@gmail.com>
2026-02-16 11:04:29 -0800

    testHealthDbt
    allow all domain

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
   server: {
     hot: true,
-    allowedHosts: true
+    allowedHosts: true,
+    port: parseInt(env.VITE_PORT) || 5000
     // allowedHosts: [
     //   'health.localhost.mydomain.com'
     // ]
-  }
+  },
+  define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
 })
